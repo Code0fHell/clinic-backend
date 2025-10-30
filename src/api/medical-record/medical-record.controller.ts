429defff363ecbb6a64e8f34130a43ea6424363d
50nBu1 <buisonlmht@gmail.com>
2025-10-31 04:52:13 +0700

    refactor: add auth for API

--- src/api/medical-record/medical-record.controller.ts
+++ src/api/medical-record/medical-record.controller.ts
@@ -1,11 +1,20 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { MedicalRecordService } from './medical-record.service';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { RolesGuard } from 'src/common/guards/roles.guard';
+import { AuthGuard } from '@nestjs/passport';
+import { UserRole } from 'src/shared/enums/user-role.enum';
+import { Roles } from 'src/common/guards/roles.decorator';
 
+@ApiTags('medical-record')
+@ApiBearerAuth()
+@UseGuards(AuthGuard('jwt'), RolesGuard)
 @Controller('api/v1/medical-record')
 export class MedicalRecordController {
   constructor(private readonly medicalRecordService: MedicalRecordService) {}
 
   @Get('patient/:patientId')
+  @Roles(UserRole.DOCTOR, UserRole.PATIENT)
   async getByPatient(@Param('patientId') patientId: string) {
     return this.medicalRecordService.getByPatientId(patientId);
   }
